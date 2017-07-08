42524cb7be8ba398fc7ef269ff6579fc80c945f9
Sudhanva MG <mgsudhanva@gmail.com>
2017-07-08 23:49:46 +0530

    backend integrated

--- web/app/js/core/controllers/SignupCtrl.js
+++ web/app/js/core/controllers/SignupCtrl.js
@@ -1,9 +1,20 @@
 app.controller('SignCtrl', ['$scope', '$state', 'authUser', function($scope, $state, authUser) {
     $scope.showitems = true;
-    $scope.items = [];
-    $scope.onAddItem = function() {
-        $scope.showitems = false;
-    }
+    $scope.items = [{
+        name: "joey",
+        item: "chandler",
+        quantity: 4,
+        amount: 6,
+    }];
+    $(function() {
+        authUser.getApprovedItems().then(function(data) {
+                console.log(data);
+                $scope.items = data.data;
+            },
+            function() {
+                console.log("error");
+            });
+    })
     $scope.onAddedItem = function() {
         $scope.showitems = true;
         var newItem = {
@@ -16,7 +27,28 @@ app.controller('SignCtrl', ['$scope', '$state', 'authUser', function($scope, $st
 
     }
     $scope.buyItem = function(itemVal) {
-        console.log("calling function");
-        authUser.testcall();
+        console.log("calling function", itemVal);
+    }
+    $scope.onAdditem = function() {
+        var newItem = {
+            name: $("#name").val(),
+            item: $("#item").val(),
+            quantity: $("#quantity").val(),
+            amount: $("#amount").val(),
+        }
+        authUser.sendForApproval(newItem).then(function(data) {
+
+                console.log("success");
+            },
+            function() {
+                console.log("error");
+            });
+        authUser.getApprovedItems().then(function(data) {
+                console.log(data);
+                $scope.items = data.data;
+            },
+            function() {
+                console.log("error");
+            });
     }
 }]);
