eab4909fe520515ae77c36f0978c20330be122df
Onam Bhartia <onam.bhartia@gmail.com>
2017-07-09 00:54:21 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -11,70 +11,130 @@ router.get('/welcome', function(req, res, next) {
 });
 
 router.get('/getitems', function (req, res){
-	      db.collection('inventory').findOne({}, function (err, dat) {
-			     if (!err && dat) {
-			        res.send(dat);
-			     }
-			     else {
-			        res.send(err);
-			     }
-			  })
-		});
+	      db.collection('inventory').find({}).toArray(function(err, data) {
+        if (!err && data) {
+            console.log(data);
+            res.send({
+                "status": "Success",
+                "data": data
+            });
+        } else {
+            res.send({
+                "status": "Failed"
+            });
+        }
+    });
+});
 router.post('/additems', function (req, res) {
-	  db.collection('inventory_to_approve').insertOne({'title':''+req.body.item,'flag':false}, function (err, dat){
-			 if(!err){
-					 res.send(dat);
-				}
-			 else if(!req){
-				 res.send('no data recieved');
-			 }
-			 else{
-				res.send(err);
-			}
-		})
+	  console.log(req.body);
+    db.collection('inventory_to_approve').insertOne({
+
+        name: req.body.name,
+        item: req.body.item,
+        quantity: req.body.amount,
+        amount: req.body.quantity,
+        shopped: false,
+
+    }, function(err, data) {
+        if (!err) {
+            res.send({"status":"success"});
+        } else if (!req.body) {
+            res.send('no data recieved');
+        } else {
+            res.send({
+                "status": "Failed"
+            });
+        }
+    })
 });
 
 router.get('/admin/inventory', function (req, res) {
-	db.collection('inventory_to_approve').findOne({'flag':false}, function(err, dat){
-		if (!err) {
-			res.send(dat);
+	db.collection('inventory_to_approve').findOne({
+		name: req.body.name,
+        item: req.body.item,
+        quantity: req.body.amount,
+        amount: req.body.quantity,
+	}, function(err, dat){
+		if (!err && dat) {
+			db.collection('inventory').insertOne({}, function(err, dat1){
+				if (!err && dat1) {
+
+        	
+
+        	//check if its correct	
+        	db.collection('inventory_to_approve').remove({}, 1);
+
+            
+            res.send({"status": "success"});
+        } else if (!req.body) {
+            res.send('no data recieved');
+        } else {
+            res.send({
+                "status": "Failed"
+            });
+        }
+    })
+			else{
+				res.send({
+					"status": "Failed"
+				});
+				}
+			}
+	
 		}
 	})
 });
 
-router.post('/getitems',function (req, res){
-	db.collection('inventory_to_approve').findOne({'flag':true}, function(err, dat){
-		if (!err && dat){
-			db.collection('inventory').update({}, function(err, dat1){
-				if (!err) {
-					res.send(dat1);
-				}
+//router.post('/getitems',function (req, res){
+//	db.collection('inventory_to_approve').findOne({'flag':true}, function(err, dat){
+//		if (!err && dat){
+//			db.collection('inventory').update({}, function(err, dat1){
+//				if (!err) {
+//					res.send(dat1);
+//				}
+//			})
+//		}
+//	})
+//});
+
+router.get('/education',function (req, res){
+	db.collection('academics').find({}).toArray({}, function(err, dat){
+		if(!err && dat){
+			res.send(dat);
+		}
+		else{
+			res.send({
+				"status": "Failed"
 			})
 		}
 	})
 });
 
-router.get('/education',function (req, res){
-	db.collection('academics').findOne({}, function(err, dat){
-		if(!err && dat){
-			res.send(dat);
-		}
-	})
-});
-
 router.post('/addedu',function(req, res){
-	db.collection('academics').insertOne({}, function(err, dat){
+	db.collection('academics').insertOne({
+		attendance: req.body.attendance,
+        	dance: req.body.dance,
+        	sing: req.body.sing,
+        	sports: req.body.sports,
+	}, function(err, dat){
 		if(!err && dat){
-			res.send(res);
+			res.send({
+				"status": "success"
+			});
 		}
 		else if(!err && !dat){
 			res.send('no data input');
 		}
 		else{
-			res.send(err);
+			res.send({
+				"status": "Failed"
+			});
 		}
 	})
 });
 
+//router.post('/buy', function(req, res){
+//	db.collection()
+//})
 
 module.exports = router;
