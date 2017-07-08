2f3f063a628994853af347bb3e0c58c91e96d771
Onam Bhartia <onam.bhartia@gmail.com>
2017-07-08 19:47:31 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var app = express();
+var cors = require('cors');
+var bodyParser = require('body-parser');
+app.use(express.static('.'));
+app.use(cors());
+app.use(bodyParser());
+var port = 5000;
+
+loopCounter = 0;
+
+var MongoClient = require('mongodb').MongoClient;
+
+dbURL = "mongodb://localhost:27017/reachinghand";
+//connecting to the database
+MongoClient.connect(dbURL, function (err, database) {
+	    if (!err) {
+				 console.log("connected to inventorydb ");
+				 db = database;
+			}
+			if(err){
+				console.log("DB connection Error");
+			}
+});
+
+app.get("/", function (req, res) {
+	    res.sendFile(__dirname + '/home.html');
+})
+
+app.post('/inventory', function (req, res){
+	  // Get the inventory collection
+		 
+		// Find the items
+	   db.collection('inventory').findOne({}, function (error, res) {
+			      if (!error && res) {
+							console.log(res);
+						}
+			      else {
+							console.log(error);
+						}
+				})
+	 });
+
+
