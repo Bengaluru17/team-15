204faaa56fc01a57d0a63b498d8f4e383415474a
Sudhanva MG <mgsudhanva@gmail.com>
2017-07-08 19:43:49 +0530

    items added

--- web/app/js/core/controllers/SignupCtrl.js
+++ web/app/js/core/controllers/SignupCtrl.js
@@ -1,5 +1,10 @@
 app.controller('SignCtrl', ['$scope', '$state', 'authUser', function($scope, $state, authUser) {
     $scope.showitems = true;
+    $scope.items = [{
+        name: "item1",
+        amount: "5",
+        data: "a failjfoaisjf asfjsad;ifj lmdf;idhfiads fj"
+    }];
     $scope.onAddItem = function() {
         $scope.showitems = false;
     }
