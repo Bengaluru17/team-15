5d3526dccf32d8d93f19d108cdaee13b0c7b48ed
Sudhanva MG <mgsudhanva@gmail.com>
2017-07-08 17:24:51 +0530

    flow

--- Ionic/src/pages/hello-ionic/hello-ionic.ts
+++ Ionic/src/pages/hello-ionic/hello-ionic.ts
@@ -1,23 +1,34 @@
 import { Component } from '@angular/core';
+import { Platform, MenuController, Nav } from 'ionic-angular';
+import { ViewChild } from '@angular/core';
+
+import { ListPage } from '../list/list';
 
 @Component({
   selector: 'page-hello-ionic',
   templateUrl: 'hello-ionic.html'
 })
 export class HelloIonicPage {
+  @ViewChild(Nav) nav: Nav;
+  showlists : boolean;
   icons: string[];
-  items: Array<{title: string, note: string, icon: string}>;
+  items: Array<{title: string, note: string}>;
   constructor() {
-    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    'american-football', 'boat', 'bluetooth', 'build'];
+    this.icons = ['flask', 'wifi', 'beer', 'football'];
+    this.showlists = false;
+    this.items = [
+      {
+        title: "hello",
+        note :"note1"
+      }
+    ];
+   
+  }
 
-    this.items = [];
-    for(let i = 1; i < 11; i++) {
-      this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-      });
-    }
+  onAdd(){
+    this.showlists = false;
+  }
+  onAdded(){
+    this.showlists = true;
   }
 }
