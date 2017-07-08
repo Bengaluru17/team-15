44d303ea5b79d1b6628a6790cecb8f424112ccec
Sudhanva MG <mgsudhanva@gmail.com>
2017-07-08 20:21:09 +0530

    backend node

--- web/app/js/core/controllers/SignupCtrl.js
+++ web/app/js/core/controllers/SignupCtrl.js
@@ -1,14 +1,22 @@
 app.controller('SignCtrl', ['$scope', '$state', 'authUser', function($scope, $state, authUser) {
     $scope.showitems = true;
-    $scope.items = [{
-        name: "item1",
-        amount: "5",
-        data: "a failjfoaisjf asfjsad;ifj lmdf;idhfiads fj"
-    }];
+    $scope.items = [];
     $scope.onAddItem = function() {
         $scope.showitems = false;
     }
     $scope.onAddedItem = function() {
         $scope.showitems = true;
+        var newItem = {
+            name: $("#name").val(),
+            item: $("#item").val(),
+            quantity: $("#quantity").val(),
+            amount: $("#amount").val(),
+        }
+        $scope.items.push(newItem);
+
+    }
+    $scope.buyItem = function(itemVal) {
+        console.log("calling function");
+        authUser.testcall();
     }
 }]);
