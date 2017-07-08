f0b7e0246ef41b76e2ed7bfda4a3de4dd16c3196
Sudhanva MG <mgsudhanva@gmail.com>
2017-07-08 19:24:05 +0530

    page flow

--- web/app/js/core/controllers/LandCtrl.js
+++ web/app/js/core/controllers/LandCtrl.js
@@ -1,10 +1,10 @@
-app.controller('LandCtrl', ['$scope','$state','authUser', function($scope, $state,authUser) {
+app.controller('LandCtrl', ['$scope', '$state', 'authUser', function($scope, $state, authUser) {
 
 
-  $scope.login = function(){
-    $state.go('app');
-  };
-  $scope.signup = function(){
-    $state.go('signup');
-  };
+    $scope.login = function() {
+        $state.go('app');
+    };
+    $scope.itemsPortal = function() {
+        $state.go('itemsPortal');
+    };
 }]);
