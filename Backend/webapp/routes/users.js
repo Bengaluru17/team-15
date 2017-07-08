6da91a6930ced2e0427d2bce41f6f9463ea3ab93
Sudhanva MG <mgsudhanva@gmail.com>
2017-07-08 20:42:11 +0530

    mongosetup

--- Backend/webapp/routes/users.js
+++ Backend/webapp/routes/users.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    res.send('respond with a resource');
+});
+
+router.get('/home', function(req, res, next) {
+    res.send('welcome to home');
+    db.addone({});
 });
 
 module.exports = router;
