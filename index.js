00fd3a50e7509c38d8a9cfc1589cb717ea6829c2
Onam Bhartia <onam.bhartia@gmail.com>
2017-07-08 22:43:48 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -11,22 +11,19 @@ router.get('/welcome', function(req, res, next) {
 });
 
 router.get('/getitems', function (req, res){
-	    // Get the inventory collection
-			//
-			//     // Find the items
-			  db.collection('inventory').findOne({}, function (err, res) {
-			     if (!error && res) {
-			        console.log(res);
+	      db.collection('inventory').findOne({}, function (err, res) {
+			     if (!err && res) {
+			        res.send(res);
 			     }
 			     else {
-			        console.log(err);
+			        res.send(err);
 			     }
 			  })
 		});
 router.post('/additems', function (req, res) {
-	  db.collection('inventory').addOne({'title':''+req.body.item}, function (err, res){
-			 if(!error){
-					 console.log(res);
+	  db.collection('inventory_to_approve').addOne({'title':''+req.body.item,'flag':false}, function (err, res){
+			 if(!err){
+					 res.send(res);
 				}
 			 else if(!req){
 				 res.send('no data recieved');
@@ -37,5 +34,25 @@ router.post('/additems', function (req, res) {
 		})
 });
 
-	                                                                                                 
+router.get('/admin/inventory', function (req, res) {
+	db.collection('inventory_to_approve').findOne({'flag':false}, function(err, res){
+		if (!err) {
+			res.send(res);
+		}
+	})
+});
+
+router.post('/getitems',function (req, res){
+	db.collection('inventory_to_approve').findOne({'flag':true}, function(err, res){
+		if (!err && res){
+			db.collection('inventory').update({}, function(err, res){
+				if (!err) {
+					res.send(res);
+				}
+			})
+		}
+	})
+});
+
+                 
 module.exports = router;
