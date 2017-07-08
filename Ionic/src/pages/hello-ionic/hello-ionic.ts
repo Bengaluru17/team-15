a8de2e5ede7cd7e128e795291c89515b35d9ea29
Sudhanva MG <mgsudhanva@gmail.com>
2017-07-08 16:26:19 +0530

    ionic updated

---
+++ Ionic/src/pages/hello-ionic/hello-ionic.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'page-hello-ionic',
+  templateUrl: 'hello-ionic.html'
+})
+export class HelloIonicPage {
+  icons: string[];
+  items: Array<{title: string, note: string, icon: string}>;
+  constructor() {
+    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
+    'american-football', 'boat', 'bluetooth', 'build'];
+
+    this.items = [];
+    for(let i = 1; i < 11; i++) {
+      this.items.push({
+        title: 'Item ' + i,
+        note: 'This is item #' + i,
+        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
+      });
+    }
+  }
+}
