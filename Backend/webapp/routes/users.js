61f6684ebc1584bcc462e435a1354121370797e9
Sudhanva MG <mgsudhanva@gmail.com>
2017-07-08 20:54:13 +0530

    backend routes/approve

--- Backend/webapp/routes/users.js
+++ Backend/webapp/routes/users.js
@@ -18,6 +18,10 @@ router.get('/users/panel', function(req, res, next) {
     res.send('respond with a ohhhh');
 });
 
+router.get('/users/approve', function(req, res, next) {
+    res.send('respond with a ohhhh');
+});
+
 
 
 module.exports = router;
